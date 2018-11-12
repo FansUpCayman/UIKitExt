be84f45ce11af6456584a5b7739e9ea24fecd672
Javier Zhang <wordlessj@gmail.com>
2018-11-12 14:42:38 +0800

    Add UICollectionView.headerView

--- Sources/UIKit/UICollectionView.swift
+++ Sources/UIKit/UICollectionView.swift
@@ -69,3 +69,30 @@ extension UICollectionView: CellContainer {
         ) as! View
     }
 }
+
+private let headerViewKey = AssociatedKey<UIView>()
+
+@available(iOS 9.0, *)
+extension UICollectionView {
+    public var headerView: UIView? {
+        get { return self[headerViewKey] }
+        set { setHeaderView(newValue) }
+    }
+
+    public func setHeaderView(_ view: UIView?, insets: UIEdgeInsets = .zero) {
+        contentInset.top = 0
+        headerView?.removeFromSuperview()
+        self[headerViewKey] = view
+
+        guard let view = view else { return }
+        let size = view.systemLayoutSizeFitting(UIView.layoutFittingExpandedSize)
+        contentInset.top = size.height + insets.vertical
+        view.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(view)
+
+        let xOffset = (insets.left - insets.right) / 2
+        view.centerXAnchor.constraint(equalTo: centerXAnchor, constant: xOffset).isActive = true
+        view.widthAnchor.constraint(equalTo: widthAnchor, constant: -insets.horizontal).isActive = true
+        view.bottomAnchor.constraint(equalTo: topAnchor, constant: -insets.bottom).isActive = true
+    }
+}
