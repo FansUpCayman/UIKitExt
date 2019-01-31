25dbf18d7f82997f2b3959f6ff1acacae28a6859
Javier Zhang <wordlessj@gmail.com>
2019-01-31 14:20:09 +0800

    Add FlatRepeated

---
+++ Sources/Foundation/FlatRepeated.swift
@@ -0,0 +1,56 @@
+//
+//  FlatRepeated.swift
+//  UIKitExt
+//
+//  Copyright (c) 2019 Javier Zhang (https://wordlessj.github.io/)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+public struct FlatRepeated<Base: RandomAccessCollection> where Base.Index: BinaryInteger {
+    private let base: Base
+    private let repeatCount: Int
+
+    public init(_ base: Base, count: Int) {
+        self.base = base
+        repeatCount = count
+    }
+}
+
+extension FlatRepeated: RandomAccessCollection {
+    public var startIndex: Base.Index { return base.startIndex }
+
+    public var endIndex: Base.Index {
+        return base.startIndex + numericCast(repeatCount * base.count)
+    }
+
+    public subscript(position: Base.Index) -> Base.Element {
+        return base[startIndex + (position - startIndex) % numericCast(base.count)]
+    }
+
+    public func index(after i: Base.Index) -> Base.Index {
+        return base.index(after: i)
+    }
+
+    public func index(before i: Base.Index) -> Base.Index {
+        return base.index(before: i)
+    }
+}
